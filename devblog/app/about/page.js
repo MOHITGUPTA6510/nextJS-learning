2d79b98bf10e368ac9be79a3d2c73632c867b24c
Mohit Gupta <mohitgupta@MacBookPro.lan>
2026-08-24 19:20:50 +0530

    Initializing the app

---
+++ devblog/app/about/page.js
@@ -0,0 +1,8 @@
+export default function About(){
+    return (
+        <div>
+            <h1>About DevBlog</h1>
+            <p>DevBlog is a simple blog where we learn modren web devlopment.</p>
+        </div>
+    );
+}
---
+++ devblog/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return (
+        <>
+            <div>
+                <nav>
+                    <title>Devblog</title>
+                </nav>
+            </div>
+        </>
+    );
+}
