4a7b74e0066680e0f832beecd4276d3768c24e8d
Mohit Gupta <mohitgupta@MacBookPro.lan>
2026-08-24 18:29:43 +0530

    restarting the project

--- devblog/app/blog/loading.jsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <main className="loading-page">
-      <div className="loader"></div>
-
-      <p>Loading blog posts...</p>
-    </main>
-  );
-}
