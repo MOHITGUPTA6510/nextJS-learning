adc0fb8348f6c53644194800451a1e5a3fa9bf9a
Mohit Gupta <mohitgupta@MacBookPro.lan>
2026-08-27 02:18:34 +0530

    navbar is added

--- devblog/components/Navbar.jsx
+++ devblog/components/Navbar.jsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 export default function Navbar(){
     return (
         <>
             <div>
-                <nav>
-                    <title>Devblog</title>
+                <nav className = "navbar">
+                    <div className ="nav-logo"> DevBlog </div>
+                    <div className ="nav-links">
+                        <Link href ="/">Home</Link>
+                        <Link href ="/about">About</Link>
+                        <Link href ="/contact">Contact</Link>
+                        
+                    </div>
                 </nav>
             </div>
         </>
