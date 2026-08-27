9565cf2d5220adfdc732cde1d46e5dfa47db8728
Mohit Gupta <mohitgupta@MacBookPro.lan>
2026-08-28 02:00:17 +0530

    add author and data for more info

--- devblog/app/blog/[slug]/page.js
+++ devblog/app/blog/[slug]/page.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import {blogs} from "@/data/blogs";
 
 export default async function SlugPost({params}){
+    console.log("Running on the server");
     
     const { slug } = await params;
     const data = blogs.find((blog) => {
@@ -16,7 +18,11 @@ export default async function SlugPost({params}){
         
         <div>
             <h1>{data.heading}</h1>
+            <p>By {data.author}</p>
+            <p>{data.date}</p>
             <p>{data.description}</p>
+            <p>{data.content}</p>
+            <Link href="/blog">Back to blog</Link>
         </div>
         
     );
--- devblog/data/blogs.js
+++ devblog/data/blogs.js
@@ -4,20 +4,26 @@ export const blogs = [
       heading: "Next.js routing",
       description: "Learning about the routing ....",
       slug : "nextjs-routing",
-      content : "..."
+      content : "Next.js provides a powerful routing system based on the file and folder structure. With the App Router, folders represent URL segments and page.js files represent the pages. Dynamic routes such as [slug] allow us to create multiple pages using a single page component.",
+      author: "Mohit",
+      date: "August 28, 2026"
     },
     {
       id : 2,
       heading: "React Components",
       description: "Learning about the react components ....",
       slug : "react-components",
-      content : "..."
+      content : "React components are reusable pieces of UI. A component can receive data through props and use that data to display different content. Breaking a website into smaller components makes the application easier to understand, maintain, and reuse.",
+      author : "Mohit",
+      date : "August 28, 2026"
     },
     {
       id : 3,
       heading: "JavaScript Fundamentals",
       description: "Learning about the javascript fundamentals  ....",
       slug : "javascript-fundamentals",
-      content : "..."
+      content : "JavaScript is a programming language used to make web applications interactive. Important fundamentals include variables, functions, arrays, objects, loops, conditions, and array methods such as map and find. These concepts are also heavily used when building React and Next.js applications.",
+      author : "Mohit",
+      date : "August 28, 2026"
     }
   ]
