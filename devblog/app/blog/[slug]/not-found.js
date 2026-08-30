aa9266495dda1dec5fa6ed7ce202516519a15950
Mohit Gupta <mohitgupta@MacBookPro.lan>
2026-08-30 14:04:38 +0530

    add not found page

---
+++ devblog/app/blog/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return(
+        <>
+            <h3>Article Not Found </h3>
+            <p>The article you're looking for doesn't exist.</p>
+        </>
+    );
+}
