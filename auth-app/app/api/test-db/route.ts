b191977e78f08f546b09c000097fa4c961068420
Mohit Gupta <mohitguptakvs@gmail.com>
2026-09-10 05:17:53 +0530

    adding try and catch to handle error

--- auth-app/app/api/test-db/route.ts
+++ auth-app/app/api/test-db/route.ts
@@ -1,6 +1,5 @@
 import { db } from "@/lib/db";
 import bcrypt from "bcrypt";
-import { Passions_Conflict } from "next/font/google";
 
 export async function POST(request: Request) {
     const body = await request.json();
@@ -14,32 +13,50 @@ export async function POST(request: Request) {
         );
     }
 
-    if(password.length >8){
+    try{
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return Response.json(
+                { error: "Invalid email address" },
+                { status: 400 }
+            );
+        }
+
+        if(password.length <8){
+            return Response.json(
+                {error : "Password must be at least 8 charecters "},
+                {status : 400}
+            );
+        }
+        
+        const existingUser = await db.query(
+            "SELECT id FROM users WHERE email = $1",
+            [email]
+        );
+        if(existingUser.rows.length>0){
+            return Response.json(
+                {error: "Email already registered"},
+                {status : 409}
+            );
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const result = await db.query(
+            `INSERT INTO users (name, email, password_hash)
+            VALUES ($1, $2, $3)
+            RETURNING id, name, email`,
+            [name, email, passwordHash]
+        );
+
+        return Response.json(result.rows[0]);
+    }catch(error){
+        console.error(error);
         return Response.json(
-            {error : "Password must be at least 8 charecters "},
-            {status : 400}
+            {error : "Something went wrong"},
+            {status : 500}
         );
     }
-    
-    const existingUser = await db.query(
-        "SELECT id FROM user WHERE email = $1",
-        [email]
-    );
-    if(existingUser.rows.length>0){
-        return Response.json(
-            {error: "Email already registered"},
-            {status : 409}
-        );
-    }
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const result = await db.query(
-        `INSERT INTO users (name, email, password_hash)
-         VALUES ($1, $2, $3)
-         RETURNING id, name, email`,
-        [name, email, passwordHash]
-    );
-
-    return Response.json(result.rows[0]);
 }
