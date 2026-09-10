2e1ca3ce2b2d8b83d84f413e00c7fdedf50acb0d
Mohit Gupta <mohitguptakvs@gmail.com>
2026-09-10 18:45:31 +0530

    added forget message

---
+++ auth-app/app/api/forget-password/route.ts
@@ -0,0 +1,48 @@
+import { db } from "@/lib/db";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
+export async function POST(request: Request) {
+    const body = await request.json();
+
+    const { email } = body;
+
+    if (!email) {
+        return Response.json(
+            { error: "Email is required" },
+            { status: 400 }
+        );
+    }
+
+    const result = await db.query(
+        "SELECT id FROM users WHERE email = $1",
+        [email]
+    );
+
+    if (result.rows.length === 0) {
+        return Response.json(
+            { error: "Account not found" },
+            { status: 404 }
+        );
+    }
+
+    const user = result.rows[0];
+
+    const otp = crypto.randomInt(100000, 1000000).toString();
+
+    const otpHash = await bcrypt.hash(otp, 10);
+
+    const expiresAt = new Date();
+    expiresAt.setMinutes(expiresAt.getMinutes() + 10);
+
+    await db.query(
+        `INSERT INTO password_reset_otps
+        (user_id, otp_hash, expires_at)
+        VALUES ($1, $2, $3)`,
+        [user.id, otpHash, expiresAt]
+    );
+
+    return Response.json({
+        message: "OTP generated successfully",
+    });
+}
